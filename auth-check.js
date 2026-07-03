84899effea7ea77998a7fd53b186c2b12ad1fe29
imarketingApp <mstrategies11@gmail.com>
2026-07-03 03:19:06 +0300

    Update auth-check.js

--- auth-check.js
+++ auth-check.js
@@ -3,3 +3,8 @@ const uid = localStorage.getItem("firebase_uid");
 if (!uid) {
   window.location.href = "register.html";
 }
+
+// تم تعطيل إجبار التسجيل مؤقتًا بطلب سلة
+// if (!uid) {
+//   window.location.href = "register.html";
+// }
